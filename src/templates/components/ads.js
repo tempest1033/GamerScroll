74ff86be84261a70c78ea0309de1303d3bdd303c
JYP <tempest1033@gmail.com>
2026-01-11 14:26:50 +0900

    2026-01-11 14:27 모바일 상단 광고 min-width:320px 적용

--- src/templates/components/ads.js
+++ src/templates/components/ads.js
@@ -7,7 +7,7 @@ function renderMobileTopAd(slotId) {
   if (!slotId) return '';
   return `<div class="ad-card ad-card-mobile-top ad-mobile-only">
   <ins class="adsbygoogle"
-       style="display:inline-block;width:320px;height:100px"
+       style="display:inline-block;min-width:320px;height:100px"
        data-ad-client="${ADSENSE_CLIENT}"
        data-ad-slot="${slotId}"
        data-ad-format="horizontal"
