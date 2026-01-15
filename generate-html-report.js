3580015e2f800f400fe2669c29eba6729d71f03d
JYP <tempest1033@gmail.com>
2026-01-16 02:36:51 +0900

    모바일 CNAME 생성 제거 (Cloudflare Pages 사용)

--- generate-html-report.js
+++ generate-html-report.js
@@ -1057,14 +1057,13 @@ ${sitemapEntries}
   fs.writeFileSync(`${DOCS_DIR}/sitemap.xml`, sitemapXml, 'utf8');
   console.log(`📍 Sitemap 생성: 메인 ${mainPages.length}개 + 게임 ${gamePages.length}개 + 트렌드 ${trendPages.length}개 = 총 ${allPages.length}개 URL`);
 
-  // 모바일 빌드 시 CNAME, robots.txt 생성
+  // 모바일 빌드 시 robots.txt 생성 (CNAME 불필요 - Cloudflare Pages 사용)
   if (isMobileBuild) {
-    fs.writeFileSync(`${DOCS_DIR}/CNAME`, 'm.gamerscrawl.com', 'utf8');
     fs.writeFileSync(`${DOCS_DIR}/robots.txt`, `User-agent: *
 Allow: /
 
 Sitemap: https://m.gamerscrawl.com/sitemap.xml`, 'utf8');
-    console.log('📱 모바일: CNAME, robots.txt 생성');
+    console.log('📱 모바일: robots.txt 생성');
   }
 
   // Service Worker 캐시 버전 자동 업데이트 (빌드마다 새 버전)
