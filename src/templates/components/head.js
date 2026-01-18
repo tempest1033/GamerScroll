8e67c4b17258db8cde4e6b8b183d79dde11b4bec
JYP <tempest1033@gmail.com>
2026-01-18 22:35:22 +0900

    2026-01-18 22:25 AdSense init simplify (Google standard)

--- src/templates/components/head.js
+++ src/templates/components/head.js
@@ -216,12 +216,8 @@ function generateHead(options = {}) {
 		  <link rel="preload" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/packages/pretendard/dist/web/static/woff2/Pretendard-Bold.woff2" as="font" type="font/woff2" crossorigin>
 		  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css">
 			  <link rel="stylesheet" href="/styles.css">
-	  <!-- AdSense 스크립트 (async 권장) -->
-	  <script>
-	    window.adsbygoogle = window.adsbygoogle || [];
-	    window.gcAdsSDKLoaded = window.gcAdsSDKLoaded || false;
-	  </script>
-	  <script async fetchpriority="high" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9477874183990825" crossorigin="anonymous" onload="window.gcAdsSDKLoaded=true; if (typeof window.gcOnAdsSDKLoad === 'function') window.gcOnAdsSDKLoad();"></script>
+	  <!-- AdSense 스크립트 (Google 표준) -->
+	  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9477874183990825" crossorigin="anonymous"></script>
 		  <script async fetchpriority="low" src="https://unpkg.com/twemoji@14.0.2/dist/twemoji.min.js" crossorigin="anonymous"></script>
 	  <!-- Firebase Analytics (프로덕션만) -->
 	  <script>
