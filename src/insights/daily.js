47edd260ce764c782bb27668a46ca53adf0222c1
JYP <tempest1033@gmail.com>
2026-01-30 23:39:37 +0900

    2026-01-30 23:38 Fix daily insight CSS path to absolute

--- src/insights/daily.js
+++ src/insights/daily.js
@@ -325,7 +325,7 @@ function generateInsightHTML(insight) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Daily Insight - ${date} | GAMER SCROLL</title>
-  <link rel="stylesheet" href="../styles.css">
+  <link rel="stylesheet" href="/styles.css">
 </head>
 <body class="page-daily-insight">
   <div class="insight-container">
