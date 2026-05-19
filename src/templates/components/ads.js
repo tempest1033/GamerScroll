7705edd7af08b745be7e58957ab3fd1484c35e2d
JYP <tempest1033@gmail.com>
2026-05-19 23:22:18 +0900

    2026-05-19 23:15 모바일 top ins 에 data-ad-format=horizontal + full-width-responsive=false 추가 (PC slot 패턴 적용해 fill 살림)

--- src/templates/components/ads.js
+++ src/templates/components/ads.js
@@ -106,7 +106,9 @@ function renderMobileOnlyHomeAd(slotId) {
   <ins class="adsbygoogle ${styleId}"
        style="display:block;width:320px;height:100px"
        data-ad-client="${ADSENSE_CLIENT}"
-       data-ad-slot="${slotId}"></ins>
+       data-ad-slot="${slotId}"
+       data-ad-format="horizontal"
+       data-full-width-responsive="false"></ins>
   ${renderEagerAdPushScript()}
 </div>`;
 }
